8640905986747ca5092fe6b8e2c9130d5ba29398
sahrulanwar01 <denanawar2001@gmail.com>
2024-09-24 15:15:25 +0700

    enam

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -2,7 +2,6 @@ import ListProduct from "@/components/listProduct";
 // import { getServerSession } from "next-auth";
 // import { authOptions } from "@/lib/auth";
 // import Login from "@/components/login";
-import { HandleGetKategori } from "@/service/handleGetKategori";
 
 export const dynamic = 'force-dynamic'
 
@@ -24,10 +23,28 @@ export async function GetListProduct() {
     console.log(error);
   }
 }
+export async function GetListKategoriProduct() {
+  try {
+    const res = await fetch(`${process.env.NEXT_PUBLIC_URL}/api/get/getKategoriProduct`, {
+      method: 'GET',
+      headers: {
+        'Content-Type': 'application/json',
+        'Authorization': `${process.env.NEXT_PUBLIC_SECREET}`
+      },
+      next: {
+        revalidate: 0
+      }
+    })
+    return res.json()
+
+  } catch (error) {
+    console.log(error);
+  }
+}
 
 export default async function Home() {
   const dataList = await GetListProduct()
-  const dataKategori = await HandleGetKategori()
+  const dataKategori = await GetListKategoriProduct()
   // const session = await getServerSession(authOptions)
 
   return (
--- src/app/post/page.jsx
+++ src/app/post/page.jsx
@@ -6,9 +6,29 @@ import { HandleGetKategori } from "@/service/handleGetKategori";
 
 export const dynamic = 'force-dynamic'
 
+export async function GetListKategoriProduct() {
+    try {
+        const res = await fetch(`${process.env.NEXT_PUBLIC_URL}/api/get/getKategoriProduct`, {
+            method: 'GET',
+            headers: {
+                'Content-Type': 'application/json',
+                'Authorization': `${process.env.NEXT_PUBLIC_SECREET}`
+            },
+            next: {
+                revalidate: 0
+            }
+        })
+        return res.json()
+
+    } catch (error) {
+        console.log(error);
+    }
+}
+
 export default async function Page() {
     // const session = await getServerSession(authOptions)
-    const dataKategori = await HandleGetKategori()
+    const dataKategori = await GetListKategoriProduct()
+
     return (
         <>
             <FormInput dataKategori={dataKategori?.data} />
