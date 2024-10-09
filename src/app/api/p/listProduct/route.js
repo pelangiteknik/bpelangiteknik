0fbf10098668a7f774081e4dc0e1634c5f728202
sahrulanwar01 <denanawar2001@gmail.com>
2024-10-09 15:25:46 +0700

    sebelas

--- src/app/api/p/listProduct/route.js
+++ src/app/api/p/listProduct/route.js
@@ -14,6 +14,7 @@ export async function GET(req) {
             id: true,
             start: true,
             end: true,
+            productName: true,
             slugProduct: true,
             stockProduct: true,
             viewProduct: true,
